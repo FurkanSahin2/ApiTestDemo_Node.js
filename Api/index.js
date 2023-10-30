79ea7af6c720ecad42fa1f36ff54479b56cb9a71
Furkan Sahin <aliince.000000@gmail.com>
2023-10-30 14:57:30 +0300

    30.10.2023

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,7 @@ const ApiService = require('./ApiService');
 const path = require('path')
 
 const express = require("express");
+const { name } = require('ejs');
 const app = express();
 const PORT = 5000;
 
